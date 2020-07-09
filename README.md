5fb3154305c554faebe969339cec5c5a224eaa87
RienNeVaPlus <?@rienneva.plus>
2020-07-09 02:49:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1 align="center">
+  📊 Wikifolio API
+</h1>
+
+<p align="center">
+This is an example for the <a href="https://github.com/RienNeVaPlus/wikifolio"><strong>unofficial</strong> Node.js API client</a> for <a href="https://www.wikifolio.com">Wikifolio's platform</a>.
+</p>
+
+### Run
+
+```bash
+git clone https://github.com/RienNeVaPlus/wikifolio-example
+cd wikifolio-example
+yarn install
+yarn start
---
+++ index.ts
@@ -0,0 +1,41 @@
+import prompts from 'prompts'
+import {Wikifolio} from 'wikifolio'
+
+(async () => {
+	console.warn('Your credentials will not be stored.');
+
+	const {email} = await prompts({
+		type: 'text',
+		name: 'email',
+		message: 'Wikifolio email'
+	});
+
+	const {password} = await prompts({
+		type: 'password',
+		name: 'password',
+		message: 'Wikifolio password'
+	});
+
+	const wiki = new Wikifolio({email, password});
+
+	const wikifolio = wiki.wikifolio('wfobserver');
+	console.log(await wikifolio.details());
+
+	// const wikifolio = wiki.wikifolio('wfobserver');
+	// console.log(await wikifolio.price());
+
+	// const wikifolio = wiki.wikifolio('wf01smylel');
+	// console.log(await wikifolio.trades({pageSize: 100, page: 1}));
+
+	// const wikifolio = wiki.wikifolio('wfobserver');
+	// console.log(await wikifolio.portfolio());
+
+	// const wikifolios = await wiki.search({query: 'Supervisor'});
+	// console.log(wikifolios);
+
+	// const user = wiki.user('riennevaplus');
+	// console.log(await user.details());
+
+	// const user = wiki.user('riennevaplus');
+	// console.log(await user.wikifolios());
+})();
